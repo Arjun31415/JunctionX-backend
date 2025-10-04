1a0803ec2cc631983a7ef265f368395f04a7f4b3
Arjun31415 <arjunp0710@gmail.com>
2025-10-04 21:22:39 +0200

    added custom model for hate speech detection

--- services/audio_service.py
+++ services/audio_service.py
@@ -1,8 +1,24 @@
 from models.audio_model import WhisperXTranscriber
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 transcriber = WhisperXTranscriber(model_name="large-v2")
 transcriber.load_model()
+save_directory = "models/distilbert-base-uncased-hate-speech-checker/best-weight"
+
 
 async def handle_audio_upload(file):
     transcript_result = await transcriber.transcribe(file)
+    print(transcript_result)
+    model = AutoModelForSequenceClassification.from_pretrained(save_directory)
+
+    # Load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(save_directory)
+    inferencer = pipeline("text-classification", model=model, tokenizer=tokenizer)
+    for res in transcript_result["segments"]:
+        results = inferencer(res['text'])
+
+        print(results)
+        res['label']=results[0]['label']
+        res['score'] = results[0]['score']
+    
     return transcript_result
